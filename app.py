01d3532efd3b70bb791e1eea8bbb27715ec5ba70
Erkin <erkint@gmail.com>
2025-05-21 20:37:34 +0600

    ✨ Add user_group router and set bot commands in app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import asyncio
 import os
 
 # 🧩 Third-party packages
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv, find_dotenv
 
 # 🏠 Local modules
 from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
+from common.bot_cmds_list import private
 
 # 😊 Loading environment variables
 load_dotenv(find_dotenv())
@@ -18,8 +20,10 @@ async def main():
     dp = Dispatcher()
 
     dp.include_router(user_private_router)
+    dp.include_router(user_group_router)
 
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot)
 
 
