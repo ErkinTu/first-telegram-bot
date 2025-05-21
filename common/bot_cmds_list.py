47c04da53ad9338506de346a1a9357c1fe635c8d
Erkin <erkint@gmail.com>
2025-05-21 20:38:03 +0600

    ✨ Add bot command list for private commands in bot_cmds_list.py

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,9 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command='menu', description='Меню:'),
+    BotCommand(command='help', description='Помощь'),
+    BotCommand(command='about', description='О нас:'),
+    BotCommand(command='pyment', description='Методы оплаты:'),
+    BotCommand(command='shipping', description='Методы доставки:'),
+]
